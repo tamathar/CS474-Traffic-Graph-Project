be3d84e0b7e301327d682b412a523e5225f5b5bf
Joseph Quigley <cpu.crazy@gmail.com>
2012-02-20 10:22:45 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Traffic-Graph-API
+//
+//  Created by Joseph Quigley on 2/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
