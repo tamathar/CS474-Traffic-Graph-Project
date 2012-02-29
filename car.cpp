5dcfaf03fb76bfa6a46454fd5de465abea505a2c
German Irias <gei08a@acu.edu>
2012-02-28 20:42:21 -0600

    Fixed Car class header file. Implemented part of Car class.

--- car.cpp
+++ car.cpp
@@ -1,4 +1,109 @@
+/*************************************************************************
+ * Author: German Irias
+ * Program: Car.cpp
+ * Description: This is the implementation of the Car class for
+ * 				 for the Traffic Graph API
+ *************************************************************************/
+#include <iostream>
+#include "main.cpp"
 #include "car.h"
+using namespace std;
 
-int main(){
+namespace tfg
+{
+        //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Constructors/Destructor=-=-=-=-=-=-=-=-=-=-=-
+		
+        Car::Car()
+			:myID(maxID++),
+			 accident(0),
+			 speed(0)
+		{	
+			pos = new Position;
+		}
+		
+        Car::Car(const Car & other)
+			:myID(other.myID),
+			 accident(other.accident),
+			 speed(other.speed)
+		{
+			pos = new Position;
+		}
+		
+        Car::~Car()
+		{
+			delete pos;
+		}
+        
+        //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Getters/Setters=-=-=-=-==-=-=-=-=-=-=-=-=-
+        //All getters are standard. Setters are explained individually.
+		
+		
+        int Car::GetID() const 
+        {
+            return myID;	// return id
+        }
+
+    	bool Car::GetAccident() const
+    	{
+    	    return accident;	// return accident
+    	}
+    	
+    	void Car::SetAccident(bool acc)
+    	{
+    	    accident = acc;	// set accident
+    	}
+    	
+    	int Car::GetTimestamp() const
+    	{
+    	    ;	// return timestamp
+    	}
+    	
+    	Position Car::GetPosition() const
+    	{
+    	    return *pos;	// return position
+    	}
+    	
+    	void Car::SetPosition(Position p)
+    	{
+    	    *pos = p;		// set position
+    	}
+    	
+    	int Car::GetSpeed() const
+    	{
+    	    return speed;		// return speed
+    	}
+    	
+    	void Car::SetSpeed(int sp)
+    	{
+    	    speed = sp;		// set speed
+    	}
+		/*
+		//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-Overloaders=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+		bool operator==(const Intersection &other) const
+		{
+			if(myID = other.myID)
+				return true;
+				
+			return false;
+		}
+		
+		const Intersection & operator=(const Intersection &other)
+		{
+			myID = other.myID;
+			usage = other.usage;
+			blocked = other.blocked;
+			roadCount = other.roadCount;
+			delete []roads;
+			roads = new Road[roadCount];
+			for(int i = 0; i < roadCount; i++)
+				roads[i] = other.roads[i];
+		}
+		*/
 }
+    /*const unsigned long maxID; 
+    int myID;		
+    bool accident;	
+  	int timestamp;	
+   	Position pos;	
+   	int speed;	
+   	*/
--- car.h
+++ car.h
@@ -13,25 +13,26 @@ namespace tfg
     	Car(const Car& other);
     	~Car();	// default destructor
     
-    	int GetID();	// return id
+    	int GetID() const;	// return id
     	void SetID(int id);	// set id
-    	bool GetAccident();	// return accident
-    	void SetAccident(bool accident);	// set accident
-    	int GetTimestamp();	// return timestamp
-    	void SetTimestamp(int timestamp);	// set timestamp
-    	Position GetPosition();	// return position
-    	void SetPosition(Position pos);		// set position
-    	int GetSpeed();		// return speed
-    	void SetSpeed(int speed);		// set speed
+    	bool GetAccident() const;	// return accident
+    	void SetAccident(bool acc);	// set accident
+    	int GetTimestamp() const;	// return timestamp
+    	void SetTimestamp(int ts);	// set timestamp
+    	Position GetPosition() const;	// return position
+    	void SetPosition(Position p);		// set position
+    	int GetSpeed() const;		// return speed
+    	void SetSpeed(int sp);		// set speed
     	void SpawnEvent(string type, Position pos);	// spawn an event, describing what event it was and where in the graph
     	void UpdatePosition();	// update where in the graph the car is
     	void StopTracking();	// stop updating position
     
     private:
-    	int id;		// unique id for each car in the graph
+        unsigned long maxID; //Auto-inc when creating a new Car. That way every Car has a UID
+    	int myID;		// unique id for each car in the graph
     	bool accident;	// true if it had an accident, false otherwise
     	int timestamp;	// current time
-    	Position pos;	// tells where in the graph is the car
+    	Position * pos;	// tells where in the graph is the car
     	int speed;	// tells the speed at which the car goes
     
     };
