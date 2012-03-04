34baa53dbb4cd9a5316aa7765ac0bdbdb3123e91
Joseph Quigley <cpu.crazy@gmail.com>
2012-03-04 17:55:47 -0600

    Adding compileable changes. No promises about them working

--- graph.cpp
+++ graph.cpp
@@ -6,10 +6,27 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#include <iostream>
+
 #include "graph.h"
 
 using namespace std;
+
+
+/*============================================
+ *
+ * Symbol singleton
+ *
+ ============================================*/
+
+// Global static pointer used to ensure a single instance of the class.
+Symbol* Symbol::m_pInstance = NULL;
+
+Symbol* Symbol::Get() {
+    if (!m_pInstance)   // Only allow one instance of class to be generated.
+        m_pInstance = new Symbol;
+        return m_pInstance;
+}
+
 
 /*============================================
  *
@@ -17,12 +34,14 @@ using namespace std;
  *
  ============================================*/
 Graph::Graph() {
-    numIntersections = 2;
-    intersections = new Intersection*[numIntersections];
+    numRoads = -1;
+    dataSize = 2;
+    
+    roads = new Road[dataSize];
 }
 
 Graph::~Graph() {
-    delete[] intersections;
+    delete[] roads;
 }
 
 
@@ -31,12 +50,105 @@ Graph::~Graph() {
  * Member Functions
  *
  ============================================*/
-void Graph::AddIntersection(const Intersection *intersection) {
-    cout << intersection->GetID();
+
+bool Graph::UpdateGraph() const { //Synonymn for CreateGraph
+    return CreateGraph();
 }
 
-
-void Graph::RemoveIntersection(const Intersection *intersection) {
+bool Graph::CreateGraph() const {
+    string outputFileName = "graph.data";
+    string renderedGraphFileName = "trafficgraph.png";
+    ofstream output;
+    output.open (outputFileName.c_str() );
+    if (output.is_open() ) {
+        //output << GraphToString();
+        output.close();
+    }
+    else {
+        return false;
+    }
     
+    if(fork() == 0) { //Child process
+        execl("neato", "-Tpng", outputFileName.c_str(), "-o", renderedGraphFileName.c_str() );
+        exit(0);
+    }
+    else {
+        return false;
+    }
+    
+    return true;
 }
 
+
+void Graph::AddRoad(const Road &road) {
+    numRoads++;
+    
+    cout << "AddRoad() --> numRoads = " << numRoads << endl;
+    
+    
+    if (numRoads >= dataSize) {
+        dataSize *= 2;
+        Road *newRoads = new Road[dataSize];
+        
+        for (int i=0; i<dataSize-1; i++) {
+            newRoads[i] = roads[i];
+        }
+        
+        delete [] roads;
+        roads = newRoads;
+        
+    }
+    
+    roads[dataSize] = road;
+}
+ 
+void Graph::RemoveRoad(const Road &road) {
+    for (int i=0; i<dataSize; i++) {
+        if (roads[i] == road) {
+            if (i < dataSize) {
+                roads[i] = roads[dataSize];
+            }
+            dataSize--;
+        }
+    }
+}
+
+//We have no setters for Roads or roads b/c we don't really want people
+//changing where our roads are
+Road *Graph::GetRoads() {
+    return roads;
+}
+
+//Standard getters and setters for how many roads/roads
+//we have in our system.
+int Graph::GetNumRoads() const {
+    return numRoads+1;
+}
+
+string Graph::GraphToString() const {
+    string output = "graph Road And Road Analysis {\n";
+    
+    cout << "Num roads: " << GetNumRoads() << endl;
+    
+    for (int i=0; i<GetNumRoads(); i++) {
+        
+        Road road = roads[i];
+        
+        cout << road.GetID() << endl;
+        
+        /*
+        Road *roads = Road->GetRoads();
+        
+        for (int j=0; j<Road->GetRoadCount(); j++) {
+            Road road = roads[j];
+            cout << road.GetName() << endl;
+            //output += road.GetBeginning();
+            //output += " -- "  + road.GetEnd(); 
+//            + Symbol::Get()->Name(road.GetName() )
+        }*/
+    }
+    
+    output += "}";
+    
+    return output;
+}
--- graph.h
+++ graph.h
@@ -1,78 +1,103 @@
 #ifndef Graph_h
 #define Graph_h
 #include <iostream>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
 #include "road.h"
 #include "intersection.h"
-#include "event.h"
 using namespace std;
 
-namespace tfg
-{
-	class Graph	
-	{
-		public:
-			//This will be used for rendering the graph to show different traffic condintions
-			enum Criteria
-			{
-				Usage,				//number of cars using road
-				Congestion,			//current travel time
-				AverageCongestion,	//overall travel time
-				Accidents,			//show accidents on map
-				AccidentHistory,	//How deadly is your road?
-				All					//show ALL the data  XD
-			};
-			
-			//defaults constructors will not work because of our dynamic arrays
-			Graph();
-			Graph(const Graph & other);
-			~Graph();
-			
-			
-			//This is our main function that will actually pass our data to 
-			//GraphViz and will render the graph
-			bool UpdateGraph(criteria parameter);
-			
-			//These two are mainly for creating our graph when it is first being built.
-			//probably will be called several times at the beginning of the client code
-			//and then not touched again
-			void AddIntersection(const Intersection & intersection);
-			void AddRoad(const Road & road);
-			
-			//These might not need to be used, but w/o them we have no way of removing data.
-			//could be used for what if purposes?
-			void RemoveIntersection(const Intersection & intersection);
-			void RemoveRoad(const Road & road);
-			
-			//We have no setters for Roads or Intersections b/c we don't really want ppl
-			//changing where our roads are
-			const Road * GetRoads();
-			const Intersection * GetIntersections();
-			
-			//Standard getters and setters for how many roads/intersections
-			//we have in our system.
-			int GetNumRoads();
-			void SetNumRoads();
-			int GetNumIntersections();
-			void SetNumIntersections();
-			
-		private:
-			//A dynamic array of all our roads
-			Road * roads;
-			
-			//how many roads are in our array
-			int numRoads;
-			
-			//A dynamic array of all our intersections
-			Intersection * intersections;
-			
-			//how many intersections are in our array
-			int numIntersections;
+class Symbol {
+public:
+    const string Blocked;
+    const string OneWay;
+    const string TwoWay;
+    const string Intersection;
+    const string HeavyTraffic;
+    const string ModerateTraffic;
+    const string LightTraffic;
+    const string NoTraffic;
+    
+    string Name(string &name) const { return "[label=\"" + name + "\"]"; };
+    static Symbol* Get();
+    
+private:
+    Symbol() : // Private so that it can  not be called
+    Blocked("[color=red]"),
+    OneWay("->"),
+    TwoWay("--"),
+    Intersection("[shape=point]"),
+    HeavyTraffic("[color=orange]"),
+    ModerateTraffic("[color=yellow]"),
+    LightTraffic("[color=green]"),
+    NoTraffic(LightTraffic)
+    {};
+    
+    Symbol(Symbol const&);             // copy constructor is private
+    Symbol& operator=(Symbol const&);  // assignment operator is private
+    static Symbol* m_pInstance;
 
-	}
-	
-	
-	
+};
+
+
+class Graph {
+    public:
+        //This will be used for rendering the graph to show different traffic condintions
+        enum Criteria
+        {
+            Usage,				//number of cars using road
+            Congestion,			//current travel time
+            AverageCongestion,	//overall travel time
+            Accidents,			//show accidents on map
+            AccidentHistory,	//How deadly is your road?
+            All					//show ALL the data  XD
+        };
+                
+        //defaults constructors will not work because of our dynamic arrays
+        Graph();
+        Graph(const Graph & other);
+        ~Graph();
+        
+        
+        //This is our main function that will actually pass our data to 
+        //GraphViz and will render the graph
+        bool UpdateGraph(Criteria parameter) const; //Synonymn for CreateGraph
+        bool UpdateGraph() const; //Synonymn for CreateGraph
+    
+        bool CreateGraph(Criteria parameter) const;
+        bool CreateGraph() const;
+        
+        //These two are mainly for creating our graph when it is first being built.
+        //probably will be called several times at the beginning of the client code
+        //and then not touched again
+        void AddRoad(const Road &road);
+        
+        //These might not need to be used, but w/o them we have no way of removing data.
+        //could be used for what if purposes?
+        void RemoveRoad(const Road &road);
+        
+        Road *GetRoads();
+        
+        //Standard getters and setters for how many roads/intersections
+        //we have in our system.
+        int GetNumRoads() const;
+    
+        string GraphToString() const;
+        
+    private:
+        //A dynamic array of all our intersections
+        Road *roads;
+        Intersection *intersections;
+        
+        //how many intersections are in our array
+        int dataSize;
+        int numRoads;
+    
+        int intersectionDataSize;
+        int numIntersections;
+
+};
 	
 		
-}
 #endif	
