9fae6b036e4a26b09341c31bed201779ad9458a8
German Irias <gei08a@acu.edu>
2012-04-28 11:50:41 -0500

    adding cURL and the Data class header

---
+++ cURL.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <curl/curl.h>
+
+int main() {
+  CURL *curl;
+  CURLcode curl_res;
+  CURLINFO info;
+  long http_code;
+  double c_length;  
+  FILE *tmp;
+
+  tmp=fopen("roads.xml", "w");
+  if(tmp==NULL) {
+    printf("ERROR to open file roads.xml\n");
+    exit(2);
+  }
+
+  printf("init curl session\n");
+  curl = curl_easy_init();
+  printf("set url to download\n");
+  curl_easy_setopt(curl, CURLOPT_URL, "http://www.daltrans.org/daltrans/roads.xml");
+
+  printf("set file handler to write\n");
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA,  tmp);
+
+  printf("download the file\n");
+  curl_res = curl_easy_perform(curl);
+  if(curl_res==0) {
+    printf("file downloaded\n");
+  } else {
+    printf("ERROR in dowloading file\n");
+    fclose(tmp);
+    curl_easy_cleanup(curl);
+  }
+
+  printf("get http return code\n");
+  curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+  printf("http code: %lu\n", http_code);
+
+  printf("get size of download page\n");
+  curl_easy_getinfo(curl, CURLINFO_SIZE_DOWNLOAD, &c_length);
+  printf("length: %g\n", c_length);
+
+  printf("END: close all files and sessions\n");
+  fclose(tmp);
+  curl_easy_cleanup(curl);
+
+  return 0;
+}
---
+++ data.h
@@ -0,0 +1,31 @@
+/*
+ *
+ * Created By Joseph Quigley 02/18/2011
+ *
+ */
+
+#ifndef Data_h
+#define Data_h
+#include <iostream>
+#include "position.h"
+using namespace std;
+
+namespace tfg
+{
+	class Data
+	{
+    public:
+        Data();
+
+        void ParseRoads();
+        void ParseEvents();
+        void GetRoads();
+        void GetEvents();
+
+    private:
+        Position **positions;
+
+
+	};
+}
+#endif
